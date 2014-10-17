adae4a08ca5c136b4014e450eccd4f21e74ac837
Sebastian Röder <no-spam-github@sebastianroeder.de>
2014-10-17 23:27:57 +0200

    Initial commit

---
+++ FunFacts/FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by Sebastian Röder on 17/10/14.
+//  Copyright (c) 2014 Sebastian Röder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
